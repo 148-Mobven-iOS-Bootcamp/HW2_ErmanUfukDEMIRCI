5089e2e96c88c0e5e0febd76852db0726402becc
Erman Ufuk Demirci <ermanufukdemirci@gmail.com>
2021-12-25 17:00:21 +0300

    closure, delegate and notification center functionalities

--- HW2/Controllers/MainViewController.swift
+++ HW2/Controllers/MainViewController.swift
@@ -8,9 +8,39 @@
 import UIKit
 
 final class MainViewController: UIViewController {
-    
+
     // MARK: - Properties
-    
+
+    private let closureLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.text = "Closure: -"
+        label.textColor = .white
+        label.textAlignment = .left
+        return label
+    }()
+
+    private let delegateLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.text = "Delegate: -"
+        label.textColor = .white
+        label.textAlignment = .left
+        return label
+    }()
+
+    private let notificationCenterLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.text = "NotificationCenter: -"
+        label.textColor = .white
+        label.textAlignment = .left
+        return label
+    }()
+
     private let popupButton: UIButton = {
         let button = UIButton(type: .system)
         button.translatesAutoresizingMaskIntoConstraints = false
@@ -20,41 +50,115 @@ final class MainViewController: UIViewController {
         button.layer.cornerRadius = 12
         return button
     }()
-    
+
+    private var closureNumber = 1 {
+        didSet {
+            setClosureLabel(with: closureNumber)
+        }
+    }
+    private var delegateNumber = 2 {
+        didSet {
+            setDelegateLabel(with: delegateNumber)
+        }
+    }
+    private var notificationCenterNumber = 3 {
+        didSet {
+            setNotificationCenterLabel(with: notificationCenterNumber)
+        }
+    }
+
     // MARK: - Lifecycle
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         setupViews()
         setupConstraints()
+        setupObservers()
     }
-    
+
+    private func setupObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(onPopupViewNotification(notification:)),
+                                               name: PopupViewController.notificationCenterNumberNotificationName,
+                                               object: nil)
+    }
+
     // MARK: - UI
-    
+
     private func setupViews() {
         view.backgroundColor = .systemGray
-        popupButton.addTarget(self, action: #selector(onPopupButton), for: .touchUpInside)
+        popupButton.addTarget(self, action: #selector(onPopupButtonPressed), for: .touchUpInside)
+
+        setDelegateLabel(with: delegateNumber)
+        setClosureLabel(with: closureNumber)
+        setNotificationCenterLabel(with: notificationCenterNumber)
     }
-    
+
     private func setupConstraints() {
-        view.addSubview(popupButton)
+        let labelStack = UIStackView(arrangedSubviews: [closureLabel, delegateLabel, notificationCenterLabel])
+        labelStack.translatesAutoresizingMaskIntoConstraints = false
+        labelStack.axis = .vertical
+        labelStack.spacing = 8
+
+        let vStack = UIStackView(arrangedSubviews: [labelStack, popupButton])
+        vStack.translatesAutoresizingMaskIntoConstraints = false
+        vStack.axis = .vertical
+        vStack.spacing = 30
+
+        view.addSubview(vStack)
         
         NSLayoutConstraint.activate([
-            popupButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            popupButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            vStack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            vStack.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+
             popupButton.heightAnchor.constraint(equalToConstant: 40),
-            popupButton.widthAnchor.constraint(equalToConstant: 200)
+            popupButton.widthAnchor.constraint(equalToConstant: 200),
         ])
     }
-    
+
+    private func setDelegateLabel(with number: Int) {
+        delegateLabel.text = "Delegate: \(number)"
+    }
+
+    private func setClosureLabel(with number: Int) {
+        closureLabel.text = "Closure: \(number)"
+    }
+
+    private func setNotificationCenterLabel(with number: Int) {
+        notificationCenterLabel.text = "NotificationCenter: \(number)"
+    }
+
     // MARK: - Action
-    
-    @objc private func onPopupButton() {
-        let popupViewController = PopupViewController()
-        present(popupViewController, animated: true) {
-            print("Presented now")
+
+    @objc private func onPopupButtonPressed() {
+        let popupViewController = PopupViewController(closureNumber: closureNumber,
+                                                      delegateNumber: delegateNumber,
+                                                      notificationCenterNumber: notificationCenterNumber)
+        popupViewController.delegate = self
+        popupViewController.closure = { [weak self] number in
+            self?.closureNumber = number
         }
+        present(popupViewController, animated: true) {
+            print("Presented now!")
+        }
+    }
+
+    @objc private func onPopupViewNotification(notification: Notification) {
+        guard let userInfo = notification.userInfo,
+              let number = userInfo["number"] as? Int else {
+            print("Number cannot be found!")
+            return
+        }
+
+        notificationCenterNumber = number
+    }
+}
+
+// MARK: - PopupViewControllerDelegate
+
+extension MainViewController: PopupViewControllerDelegate {
+    func popupViewController(didChange number: Int) {
+        delegateNumber = number
     }
 }
 
--- HW2/Controllers/PopupViewController.swift
+++ HW2/Controllers/PopupViewController.swift
@@ -7,72 +7,150 @@
 
 import UIKit
 
+protocol PopupViewControllerDelegate: AnyObject {
+    func popupViewController(didChange number: Int)
+}
+
 final class PopupViewController: UIViewController {
-    
+
     // MARK: - Properties
-    
-    private let numberOfCountsLabel: UILabel = {
+
+    static let notificationCenterNumberNotificationName = Notification.Name(rawValue: "notificationCenterNumberNotificationName")
+
+    private let closureLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "-"
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.text = "Closure: -"
         label.textColor = .black
-        label.font = UIFont.systemFont(ofSize: 30)
-        label.textAlignment = .center
+        label.textAlignment = .left
         return label
     }()
-    
+
+    private let delegateLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.text = "Delegate: -"
+        label.textColor = .black
+        label.textAlignment = .left
+        return label
+    }()
+
+    private let notificationCenterLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.text = "NotificationCenter: -"
+        label.textColor = .black
+        label.textAlignment = .left
+        return label
+    }()
+
     private let countButton: UIButton = {
         let button = UIButton(type: .system)
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("Count", for: .normal)
+        button.setTitle("Count!", for: .normal)
         button.backgroundColor = .systemYellow
         button.layer.cornerRadius = 12
         return button
     }()
-    
-    private var number: Int = 0
-    
+
+    private var clickCountNumber = 0
+    private var closureNumber: Int
+    private var delegateNumber: Int
+    private var notificationCenterNumber: Int
+
+    weak var delegate: PopupViewControllerDelegate?
+
+    var closure: ((Int) -> Void)?
+
+    // MARK: - Init
+
+    init(closureNumber: Int, delegateNumber: Int, notificationCenterNumber: Int) {
+        self.closureNumber = closureNumber
+        self.delegateNumber = delegateNumber
+        self.notificationCenterNumber = notificationCenterNumber
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     // MARK: - Lifecycle
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         setupViews()
         setupConstraints()
     }
-    
+
     // MARK: - UI
-    
+
     private func setupViews() {
         view.backgroundColor = .white
         countButton.addTarget(self, action: #selector(onCountButtonPressed), for: .touchUpInside)
-        setLabel(for: number)
+
+        setClosureLabel(with: closureNumber)
+        setDelegateLabel(with: delegateNumber)
+        setNotificationCenterLabel(with: notificationCenterNumber)
     }
-    
+
     private func setupConstraints() {
-        let vStack = UIStackView(arrangedSubviews: [numberOfCountsLabel, countButton])
+        let labelStack = UIStackView(arrangedSubviews: [closureLabel, delegateLabel, notificationCenterLabel])
+        labelStack.translatesAutoresizingMaskIntoConstraints = false
+        labelStack.axis = .vertical
+        labelStack.spacing = 8
+
+        let vStack = UIStackView(arrangedSubviews: [labelStack, countButton])
         vStack.translatesAutoresizingMaskIntoConstraints = false
         vStack.axis = .vertical
-        vStack.spacing = 40.0
-        
+        vStack.spacing = 30
+
         view.addSubview(vStack)
-        
+
         NSLayoutConstraint.activate([
             vStack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             vStack.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+
             countButton.heightAnchor.constraint(equalToConstant: 40),
             countButton.widthAnchor.constraint(equalToConstant: 200)
         ])
     }
-    
-    private func setLabel(for number: Int) {
-        numberOfCountsLabel.text = "\(number)"
+
+    private func setDelegateLabel(with number: Int) {
+        delegateLabel.text = "Delegate: \(number)"
     }
-    
+
+    private func setClosureLabel(with number: Int) {
+        closureLabel.text = "Closure: \(number)"
+    }
+
+    private func setNotificationCenterLabel(with number: Int) {
+        notificationCenterLabel.text = "NotificationCenter: \(number)"
+    }
+
     // MARK: - Action
-    
+
     @objc private func onCountButtonPressed() {
-        number += 1
-        setLabel(for: number)
+        // With closure
+        closureNumber += 1
+        closure?(closureNumber)
+
+        // With delegate
+        delegateNumber += 2
+        delegate?.popupViewController(didChange: delegateNumber)
+
+        // With NotificationCenter
+        notificationCenterNumber += 3
+        NotificationCenter.default.post(name: PopupViewController.notificationCenterNumberNotificationName,
+                                        object: nil,
+                                        userInfo: ["number": notificationCenterNumber])
+
+        setClosureLabel(with: closureNumber)
+        setDelegateLabel(with: delegateNumber)
+        setNotificationCenterLabel(with: notificationCenterNumber)
     }
 }
